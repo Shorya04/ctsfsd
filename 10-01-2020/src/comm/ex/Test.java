f646a451d83b230049922c0dfa371bc7fc13bb75
Alchemy <Alchemy@192.168.1.40>
2020-01-09 23:10:09 -0800

    update

--- 10-01-2020/src/comm/ex/Test.java
+++ 10-01-2020/src/comm/ex/Test.java
@@ -8,9 +8,9 @@ import java.util.UUID;
 public class Test {
 	public static void main(String[] args) { //throws StudentExecption {
 		Set<Student> set=new HashSet<Student>();
-		set.add(new Student(UUID.randomUUID().toString(), "Shorya", "Singh", 8.4));
+		/*set.add(new Student(UUID.randomUUID().toString(), "Shorya", "Singh", 8.4));
 		set.add(new Student(UUID.randomUUID().toString(), "Amardeep", "Kaur", 8.0));
-		set.add(new Student(UUID.randomUUID().toString(), "Anshita", "Chandel", 8.8));
+		set.add(new Student(UUID.randomUUID().toString(), "Anshita", "Chandel", 8.8));*/
 		Iterator<Student> i=set.iterator();
 			while(i.hasNext()) {
 				Student itr= i.next();
