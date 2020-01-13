c3c9b78de87c736e6e4e8ffbe74a963ee225aae2
Alchemy <Alchemy@192.168.1.40>
2020-01-13 04:30:09 -0800

    Jdbc hidding username and password

--- 13-01-2020_JDBC/src/comm/example/MySqlConnection.java
+++ 13-01-2020_JDBC/src/comm/example/MySqlConnection.java
@@ -1,13 +1,23 @@
 package comm.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MySqlConnection {
 	private Connection connection;
 	static MySqlConnection myConnection;
 	
+	public static Properties getConnectionProperties() throws IOException {
+		InputStream in=new FileInputStream("mysql.Properties");
+		Properties p=new Properties();
+		p.load(in);
+		return p;
+	}
 	public MySqlConnection() {
 	
 	}
@@ -22,10 +32,14 @@ public class MySqlConnection {
     	}
     }
       
-    public Connection getMyConnection() throws SQLException
+    public Connection getMyConnection() throws SQLException,IOException
     {
     	//DriverManager.registerDriver(driver);
-    	connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/EMPLOYEE","root","welcome");
+    	Properties p=getConnectionProperties();
+    	String url=p.getProperty("jdbc.connection.url");
+		String user=p.getProperty("jdbc.connection.user");
+		String password=p.getProperty("jdbc.connection.password");
+    	connection=DriverManager.getConnection(url,user,password);
     	return connection;
     	
     }
