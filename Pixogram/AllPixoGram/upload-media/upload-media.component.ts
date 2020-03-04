9f737949be41b555a2d90fddd6bb0ab4da22ffea
Shorya04 <40701395+Shorya04@users.noreply.github.com>
2020-03-04 02:45:17 -0800

    commit
    commit

---
+++ Pixogram/AllPixoGram/upload-media/upload-media.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-upload-media',
+  templateUrl: './upload-media.component.html',
+  styleUrls: ['./upload-media.component.css']
+})
+export class UploadMediaComponent implements OnInit {
+
+  form: FormGroup;
+  file: File;
+
+  constructor(){}
+
+  ngOnInit(){}
+
+  upload(){
+    console.log("uploaded")
+  }
+  
+
+}
